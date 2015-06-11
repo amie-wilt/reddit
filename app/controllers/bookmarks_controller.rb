3c7d3fdb34727069c18465080995f599934fe7c6
Amie Wilt <amie.wilt@gmail.com>
2015-06-11 16:17:48 -0400

    added actions to Bookmarks and Comments controllers

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,16 +1,51 @@
 class BookmarksController < ApplicationController
   def index
+    @bookmarks = Bookmark.all
   end
 
   def show
+    @bookmark = Bookmark.find(params[:id])
+    @comment = @bookmark.comments.build
   end
 
-  def _form
+  def new
+    @bookmark = Bookmark.new
   end
 
   def edit
   end
 
-  def new
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+
+    if @bookmark.save
+      redirect_to @bookmark, notice: 'Bookmark was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @bookmark.update(bookmark_params)
+      redirect_to @bookmark, notice: 'Bookmark was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to bookmarks_url, notice: 'Bookmark was successfully destroyed.'
+  end
+
+  private
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def bookmark_params
+    params.require(:bookmark).permit(:url, :title, :description)
   end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,29 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def create
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    @comment = @bookmark.comments.new(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @bookmark, notice: 'Your comment was successfully created.' }
+      else
+        format.html { render 'bookmark/show' }
+      end
+    end
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def comment_params
+    params.require(:comment).permit(:author_name, :author_email, :body)
+  end
 end
